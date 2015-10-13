1003ba0a5a2344ccf4df8322bbe07a62387c8791
Kyle Maguire <kmaguire@seas.hare@seas.harvard.edu>
2015-10-13 12:04:01 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+## Gulp Template Cache
+
+This is a [`Gulp`](http://gulpjs.com) plugin that converts HTML files into JavaScript strings that can then be quickly accessed through a global variable.
+
+## Installation and Usage
+
+In a shell:
+
+```shell
+npm install --save-dev gulp-template-cache
+```
+
+In your `gulpfile.js`:
+
+```javascript
+var templateCache = require('gulp-template-cache');
+
+gulp.task('compile:templates', function() {
+  return gulp.src('src/**/*.tpl.html')
+    .pipe(templateCache())
+    .pipe(gulp.dest('build'));
+});
+```
+
+## Options
+
+- `fileName`: The name of the JavaScript file that should be output by the plugin. Defaults to 'templates.js'.
+- `globalVariable`: The name of the global variable that should reference the template cache. Defaults to 'templateCache'.
+- `nameFunction`: A function that takes a template filepath and returns the string name of that template in the cache. Defaults to using the raw filepath.
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var pt      = require('path');
+var through = require('through2');
+var File    = require('vinyl');
+
+/**
+ * @param {Object}   [options]
+ * @param {Function} [options.fileName]
+ * @param {Function} [options.globalVariable]
+ * @param {Function} [options.nameFunction]
+ */
+module.exports = function templateCache(options) {
+    options = options || {};
+
+    options.fileName = options.fileName || 'templates.js';
+    options.globalVariable = options.globalVariable || 'templateCache';
+    options.nameFunction = options.nameFunction || nameFunction_default;
+
+    var js = '(function() {\n';
+    js += '    window[ ' + options.globalVariable + ' ] = {};\n';
+
+    return through.obj(
+        function transform(file, e, done) {
+            if (file.isBuffer()) {
+                js += '    ' + convertHtml(file.contents.toString(), file.relative);
+            }
+            done(null);
+        },
+        function flush(done) {
+            js += '}());';
+
+            //TODO: window[ options.globalVariable ] = ...;
+            this.push(new File({
+                path: options.fileName,
+                contents: new Buffer(js)
+            }));
+            done(null);
+        }
+    );
+}
+
+function nameFunction_default(templatePath) {
+    return templatePath;
+}
+
+function convertHtml(view, path, nameFunction) {
+    var wrapped = "'" + view.replace(/'/g, "\\'").replace(/\n/g, '\\n') + "'";
+    return 'window[ ' + options.globalVariable + ' ][ \'' +
+        nameFunction(path).replace(/'/g, "\\'") + '\' ] = ' + wrapped + ';\n';
+}
